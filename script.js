38a0fcfb3200ed897fa38918e82267888849e6d9
GitRealBro <regime.skylark.0y@icloud.com>
2023-07-18 19:52:40 -0400

    A bit more final.

--- script.js
+++ script.js
@@ -6,10 +6,10 @@
           // Canvas settings
         const canvas = document.getElementById("hero-lightpass");
         const context = canvas.getContext("2d");
-        const container = document.getElementById('scroll-container');
         const textElement = document.getElementById('text-element');
-        const subtextElement = document.getElementById('subtext-element')
-        const descriptionElement = document.getElementById('description')
+        const subtextElement = document.getElementById('subtext-element');
+        const descriptionElement = document.getElementById('description');
+        const buttonElement = document.getElementById('Order');
         const totalSections = 10; // Number of sections to divide the container height
         const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
@@ -43,7 +43,6 @@
         // Scroll interactions
         const html = document.getElementsByTagName('html');
         const texts = [
-            "Rejuvenate",
             'Revitalize',
             'Recover',
             'Refocus',
@@ -56,7 +55,6 @@
           ];
 
           const subtexts = [
-            "Orange, Pineapple, Lime, Ginger, Carrot, Red Apple",
             'Red Cabbage, Ginger, Black Grapes, Lime, Oranges, Red Apple',
             'Green and Red Apple, Kale, Celery, Pineapple, Lemon, Ginger',
             'Kiwi, Green Grapes, Organe, Spinach, Basil, Red Apple',
@@ -69,7 +67,6 @@
           ];
 
           const descriptions = [
-            "Rejuvenate is a revitalizing blend of fresh and vibrant flavors, carefully crafted to invigorate your senses. This delightful juice concoction combines the zesty tang of oranges, the tropical sweetness of pineapples, the tangy zest of limes, and the subtle warmth of ginger. To add a hint of earthiness, we include the refreshing essence of carrots, balanced with the crispness of red apples. With each sip of Rejuvenate, you'll experience a harmonious fusion of flavors that will leave you feeling refreshed, energized, and ready to take on the day.",
             'A complete elixir, combining the vibrant essence of red cabbage, the invigorating kick of ginger, the rich sweetness of black grapes, the zesty tang of lime, the refreshing burst of oranges, and the crispness of red apple. This revitalizing blend is designed to awaken your senses and replenish your energy, offering a harmonious fusion of flavors that will leave you feeling refreshed and revitalized.',
             'At the juice bar, the "Recover" drink is a revitalizing blend of Green and Red Apple, Kale, Celery, Pineapple, Lemon, and Ginger. This vibrant concoction combines the crisp sweetness of apples with the earthy essence of kale and the refreshing tang of pineapple. The addition of celery adds a subtle hint of freshness, while lemon provides a zesty kick. Finally, the invigorating touch of ginger adds a spicy note to complete this rejuvenating beverage. Sip on the "Recover" and feel your energy replenished as you indulge in the harmonious fusion of flavors and the nourishing benefits of these wholesome ingredients.',
             'A revitalizing and nutrient-packed drink, this vibrant concoction combines the refreshing flavors and health benefits of kiwi, green grapes, orange, spinach, basil, and red apple. The tartness of kiwi and green grapes perfectly complements the citrusy sweetness of orange and red apple, creating a harmonious blend. The addition of spinach and basil adds a dose of green goodness, providing essential vitamins and minerals. This invigorating drink not only tantalizes your taste buds but also replenishes your body with essential nutrients, helping you refocus and recharge.',
@@ -87,15 +84,28 @@
     
   
         window.addEventListener('scroll', () => {  
+          function fadeOut() {
+            textElement.style.opacity = '0';
+              descriptionElement.style.opacity = '0';
+              subtextElement.style.opacity = '0';
+              buttonElement.style.opacity = '0';
+          } 
+
+          function fadeIn () {
+            textElement.style.opacity = '1';
+                subtextElement.style.opacity = '1';
+                descriptionElement.style.opacity = '1';
+                buttonElement.style.opacity = '1';
+          }
           var myDiv = document.getElementById('scroll-container');
           var scrollPosition = window.scrollY;
-          var divOffset = (myDiv.offsetTop + (vh / 2));
-          var divHeight = myDiv.clientHeight;
+          var divOffset = (myDiv.offsetTop);
+          var divHeight = (2.5 * vh);
           
         if (scrollPosition >= divOffset && scrollPosition < divOffset + divHeight) {
             const scrollTop = (html[0].scrollTop - divOffset);
             const maxScrollTop = (scrollTop - window.innerHeight);
-            const scrollFraction = (scrollTop / (3.9 * vh));
+            const scrollFraction = ((scrollTop / divHeight));
             const frameIndex = Math.min(
                 frameCount - 1,
                 Math.floor(scrollFraction * frameCount)
@@ -104,32 +114,47 @@
 
             requestAnimationFrame(() => context.drawImage(images[frameIndex], 0, 0));
             console.log(divOffset)
-              console.log(scrollFraction)
+              
               console.log(scrollTop)
               console.log(divOffset)
               console.log(divHeight)
-              console.log(maxScrollTop)
-            const containerHeight = container.offsetHeight;
-            const sectionHeight = containerHeight / totalSections;
-            const sectionIndex = Math.floor(scrollTop / sectionHeight);
+              console.log(maxScrollTop + "hj")
+            const sectionHeight = divHeight / totalSections;
+            const sectionIndex = (Math.floor(scrollTop / sectionHeight) - 1);
+            console.log((scrollTop / sectionHeight) - (scrollFraction * 10))
+            console.log(divOffset + "  " + divHeight + "   " + scrollFraction)
 
 
-            if (((scrollFraction * 100) % 10) ==  0) {
+            if (((scrollFraction * 100) % 10) ===  0) {
+              console.log("oh?")
                 currentSectionIndex = (((scrollFraction * 10) / 10))
             }
 
-              if (sectionIndex !== currentSectionIndex) {
-                textElement.style.opacity = '0';
-                descriptionElement.style.opacity = '0';
-                subtextElement.style.opacity = '0';
+            if (((scrollFraction * 10) >= 9) && sectionIndex == 8) {
+              setTimeout(() => {
+                fadeOut()
+                canvas.style.opacity = '0';
+            }, 200);
+          }
+
+          if (((scrollFraction * 10) <= 8.5) && sectionIndex == 8) {
+            setTimeout(() => {
+              fadeIn()
+              canvas.style.opacity = '1';
+          }, 200);
+        }
+
+        console.log(scrollFraction + "hi")
+
+
+              if (currentSectionIndex !== sectionIndex) {
+                fadeOut();
                 setTimeout(() => {
                   textElement.textContent = texts[currentSectionIndex];
                   subtextElement.textContent = subtexts[currentSectionIndex];
                   descriptionElement.textContent = descriptions[currentSectionIndex];
-                  textElement.style.opacity = '1';
-                  subtextElement.style.opacity = '1';
-                  descriptionElement.style.opacity = '1';
-                
+                  fadeIn();
+                  console.log(sectionIndex)
                 }, 200); // Change the duration (in milliseconds) to control the fade in time
                 currentSectionIndex = sectionIndex;
               }
